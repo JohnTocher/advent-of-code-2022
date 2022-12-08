31f98649cb5baa35022b6f5bae1b1aa9383f08b0
John Tocher <jmtocher@gmail.com>
2022-12-09 00:00:25 +1100

    Added templates and puzzle 4 puzzle_04_input

---
+++ Puzzle_04/puzzle_04-part_1_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle 04
+
+    https://adventofcode.com/2022/day/04
+
+    John Tocher     
+    Solution to puzzle 04 part 1
+"""
+
+INPUT_FILE_NAME = "puzzle_04_input.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Tesult is: {INPUT_FILE_NAME}")
---
+++ Puzzle_Template/puzzle_nn-part_2_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle nn
+
+    https://adventofcode.com/2022/day/nn
+
+    John Tocher     
+    Solution to puzzle nn part 2
+"""
+
+INPUT_FILE_NAME = "puzzle_nn_input.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Result is: {INPUT_FILE_NAME}")
