3fff5aedbe8ac5fcb692b00747d34555e46b4860
John Tocher <jmtocher@gmail.com>
2022-12-13 00:54:59 +1100

    Added puzzle 5 part 2 solution

--- Puzzle_05/puzzle_05-part_2_jmt.py
+++ Puzzle_05/puzzle_05-part_2_jmt.py
@@ -3,12 +3,69 @@
     https://adventofcode.com/2022/day/05
 
     John Tocher     
-    Solution to puzzle 05 part 2
+    Solution to puzzle 05 part 1
 """
 
 INPUT_FILE_NAME = "puzzle_05_input.txt"
 
-with open(INPUT_FILE_NAME, "r") as input_file:
-    pass
 
-print(f"Result is: {INPUT_FILE_NAME}")
+def get_stack_states():
+    """Return the initial condition of the stacks
+
+    Each stack is modelled a list, with the first item
+    in the list rperesenting the bottom-most item
+
+    the stacks as a group are stored in a dictionary,
+    with the keys being the stack numebers
+    """
+
+    all_stacks = dict()
+    all_stacks[1] = list("PFMQWGRT")
+    all_stacks[2] = list("HFR")
+    all_stacks[3] = list("PZRVGHSD")
+    all_stacks[4] = list("QHPBFWG")
+    all_stacks[5] = list("PSMJH")
+    all_stacks[6] = list("MZTHSRPL")
+    all_stacks[7] = list("PTHNML")
+    all_stacks[8] = list("FDQR")
+    all_stacks[9] = list("DSCNLPH")
+
+    return all_stacks
+
+
+stack_state = get_stack_states()
+print(f"Initial state: {stack_state}")
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    for each_line in input_file:
+        clean_line = (
+            each_line.strip()
+        )  # This removes any whitepace from both ends,but not the middle
+        if clean_line.startswith("move"):
+
+            # The index of the letter where the word from appears
+            pos_from = clean_line.find("from")
+            # The index of the letter where the word from appears
+            pos_to = clean_line.find("to")
+
+            # get the integer values of the three things we need for each move
+            number_to_move = int(clean_line[4:pos_from])
+            stack_from = int(clean_line[pos_from + 4 : pos_to])
+            stack_to = int(clean_line[pos_to + 2 :])
+
+            # Iterating the number of crates we have to move
+            mini_stack = list()  # An empty stack representing what the crane moves
+            for stack_count in range(-number_to_move, 0):
+                # mini_stack.append(stack_state[stack_from][stack_count])
+                mini_stack.append(stack_state[stack_from].pop(stack_count))
+            print(f"From {clean_line} extending with {mini_stack}")
+            stack_state[stack_to].extend(mini_stack)
+
+# Now we will manally loop over the stack state to get the top item of each
+final_result = ""  # An empty string
+for stack_number in range(1, 10):  # There are nine crates, it will stop before 10
+    # print(f"Stack {stack_number} is {stack_state[stack_number]}")
+    this_crate = stack_state[stack_number].pop()
+    final_result += this_crate
+
+print(f"Result is: {final_result}")
--- Puzzle_05/puzzle_05_notes.md
+++ Puzzle_05/puzzle_05_notes.md
@@ -22,7 +22,13 @@ Run with:
 
 ## Part 2
 
-TBA
+This one isn't much different, other than we're now moving substacks rather than a single crate at a time.  
+To do this we'll use the "extend" list method, which puts all of the elements of one list at the end of another.  
+This is differemt to appending one list with another, which appends the second list, as a single item, to the end of the first. 
+To create the min-stack we're moving, we ise the concept of negative indexs to pop each crate we want in order, for three crates, 
+as an example, we would pop crate -3, crate -2 and finally crate -1 onto our mini-stack, then extend the target stack with that.
+
+I still have to impleement the code to read the initial state from the file.
 
 [my solution](puzzle_05-part_2_jmt.py)  
 
@@ -94,3 +100,54 @@ After the rearrangement procedure completes, what crate ends up on top of each s
 
 Your puzzle answer was TPGVQPFDH.
 
+--- Part Two ---
+
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]        
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+Your puzzle answer was DMRDFRHHH.
