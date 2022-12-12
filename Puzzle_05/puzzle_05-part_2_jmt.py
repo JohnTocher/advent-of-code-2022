ba317d511af4776f82c5ecdcdbb0d4e63e3b1aa5
John Tocher <jmtocher@gmail.com>
2022-12-12 23:38:11 +1100

    Added templates fpr puzzles 5 and 6

---
+++ Puzzle_05/puzzle_05-part_2_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle 05
+
+    https://adventofcode.com/2022/day/05
+
+    John Tocher     
+    Solution to puzzle 05 part 2
+"""
+
+INPUT_FILE_NAME = "puzzle_05_input.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Result is: {INPUT_FILE_NAME}")
