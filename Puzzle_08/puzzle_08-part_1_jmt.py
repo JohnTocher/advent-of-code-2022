1ca92c3b6b1139ca8921568373402742eb26de51
jmtocher <jmtocher@gmail.com>
2022-12-14 02:06:21 +1100

    Created template for Puzzle 08

---
+++ Puzzle_08/puzzle_08-part_1_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle 08
+
+    https://adventofcode.com/2022/day/8
+
+    John Tocher
+    Solution to puzzle 08 part 1
+"""
+
+INPUT_FILE_NAME = "puzzle_08_input.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Result is: {INPUT_FILE_NAME}")
