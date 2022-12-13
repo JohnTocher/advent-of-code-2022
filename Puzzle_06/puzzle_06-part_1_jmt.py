f45a2f3e54732b8a99f13e66a86c3f15a6e27582
jmtocher <jmtocher@gmail.com>
2022-12-13 21:29:44 +1100

    Working solution to puzzle 06 part 1

--- Puzzle_06/puzzle_06-part_1_jmt.py
+++ Puzzle_06/puzzle_06-part_1_jmt.py
@@ -8,7 +8,17 @@
 
 INPUT_FILE_NAME = "puzzle_06_input.txt"
 
-with open(INPUT_FILE_NAME, "r") as input_file:
-    pass
+running_list = list()
 
-print(f"Result is: {INPUT_FILE_NAME}")
+with open(INPUT_FILE_NAME, "r") as input_file:
+    whole_line = input_file.read()  # The first read is the t=entirety of our input.
+    for each_letter in whole_line:
+        running_list.append(each_letter)
+        if len(running_list) > 3:
+            last_group = set(running_list[-4:])
+            if len(last_group) == 4:
+                print(f"Found init string after {len(running_list)} chars")
+                break
+        # print(f"{each_letter}", end="")
+
+print(f"Result is: {len(running_list)}")
