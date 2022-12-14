c81b0df0ed8a3ceb74e63bfa046811761a4db116
jmtocher <jmtocher@gmail.com>
2022-12-14 17:51:23 +1100

    Puzzle 08 part 1 complete

--- Puzzle_08/puzzle_08-part_1_jmt.py
+++ Puzzle_08/puzzle_08-part_1_jmt.py
@@ -1,4 +1,4 @@
-""" Advent of code 2022 - Puzzle 08
+f""" Advent of code 2022 - Puzzle 08
 
     https://adventofcode.com/2022/day/8
 
@@ -8,7 +8,69 @@
 
 INPUT_FILE_NAME = "puzzle_08_input.txt"
 
-with open(INPUT_FILE_NAME, "r") as input_file:
-    pass
+tree_heights = dict()
 
-print(f"Result is: {INPUT_FILE_NAME}")
+row_num = 0  # We will use these to calculate x,y coordintaes`
+col_num = 0  # to act as keys for our dictionary, whch will contain heights
+
+
+def test_visibility(tree_to_test, adjacent_trees):
+    """Takes the co-ordinates of one tree and checks if it's taller than a list of others"""
+
+    tree_height = tree_heights[tree_to_test]
+    visibility = True
+
+    for adjacent_tree in adjacent_trees:
+        if tree_heights[adjacent_tree] >= tree_height:
+            visibility = False
+            break
+
+    return visibility
+
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    for each_line in input_file:
+        row_num += 1
+        col_num = 0
+        clean_line = each_line.strip()  # Remove whitespace
+        for each_char in clean_line:
+            col_num += 1
+            this_location = (col_num, row_num)  # A tuple like (4,3)
+            this_height = int(each_char)
+            tree_heights[this_location] = this_height
+            print(f"{this_location} : {this_height}")
+        print(f"{clean_line}")
+
+forest_width = row_num
+interior_trees_visible = 0
+
+for row_num in range(2, forest_width):  # Loops don't reach the final value
+    for col_num in range(2, forest_width):
+        this_tree = (col_num, row_num)
+        print(f"Testing: {this_tree}")
+        adjacent_trees = list()
+        for col_adj in range(1, col_num):  # left
+            adjacent_trees.append((col_adj, row_num))
+        clear_left = test_visibility(this_tree, adjacent_trees)
+        adjacent_trees = list()
+        for col_adj in range(col_num + 1, forest_width + 1):  # right
+            adjacent_trees.append((col_adj, row_num))
+        clear_right = test_visibility(this_tree, adjacent_trees)
+        adjacent_trees = list()
+        for row_adj in range(1, row_num):  # Trees above
+            adjacent_trees.append((col_num, row_adj))
+        clear_above = test_visibility(this_tree, adjacent_trees)
+        adjacent_trees = list()
+        for row_adj in range(row_num + 1, forest_width + 1):  # Trees below
+            adjacent_trees.append((col_num, row_adj))
+        clear_below = test_visibility(this_tree, adjacent_trees)
+
+        if any((clear_above, clear_below, clear_left, clear_right)):
+            interior_trees_visible += 1
+        print(f"Adjecent trees: {adjacent_trees}")
+
+
+print(f"Forest is: {forest_width} trees wide")
+print(f"Found {interior_trees_visible} interior trees visible")
+total_visible = 4 * forest_width - 4 + interior_trees_visible
+print(f"{total_visible} total visible trees")
