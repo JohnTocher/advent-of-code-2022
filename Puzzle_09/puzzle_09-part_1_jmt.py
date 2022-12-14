e08a9b9c2cb5089ff1f81507993e0ec3bab542c2
jmtocher <jmtocher@gmail.com>
2022-12-15 01:15:41 +1100

    Created Puzzle 09 templates

---
+++ Puzzle_09/puzzle_09-part_1_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle 09
+
+    https://adventofcode.com/2022/day/9
+
+    John Tocher
+    Solution to puzzle 09 part 1
+"""
+
+INPUT_FILE_NAME = "puzzle_09_input_tiny.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Result is: {INPUT_FILE_NAME}")
---
+++ Puzzle_09/puzzle_09-part_2_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle 09
+
+    https://adventofcode.com/2022/day/9
+
+    John Tocher
+    Solution to puzzle 09 part 2
+"""
+
+INPUT_FILE_NAME = "puzzle_09_input.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Result is: {INPUT_FILE_NAME}")
