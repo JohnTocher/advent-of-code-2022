2df6bf70be5b9f216630caa4fbfc1631a70045c3
John Tocher <jmtocher@gmail.com>
2022-12-16 18:01:40 +1100

    Created Puzzle 10 template

---
+++ Puzzle_10/puzzle_10-part_1_jmt.py
@@ -0,0 +1,14 @@
+""" Advent of code 2022 - Puzzle 10
+
+    https://adventofcode.com/2022/day/10
+
+    John Tocher
+    Solution to puzzle 10 part 1
+"""
+
+INPUT_FILE_NAME = "puzzle_10_input.txt"
+
+with open(INPUT_FILE_NAME, "r") as input_file:
+    pass
+
+print(f"Result is: {INPUT_FILE_NAME}")
