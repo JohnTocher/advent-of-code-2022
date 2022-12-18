279dd15c1b66ac7bdd333634428d51f09fc4fc03
John Tocher <jmtocher@gmail.com>
2022-12-18 22:51:37 +1100

    Puzzle 10 part 1 complete

--- Puzzle_10/puzzle_10-part_1_jmt.py
+++ Puzzle_10/puzzle_10-part_1_jmt.py
@@ -8,7 +8,45 @@
 
 INPUT_FILE_NAME = "puzzle_10_input.txt"
 
-with open(INPUT_FILE_NAME, "r") as input_file:
-    pass
+reg_x = 1  # Iitialise our register
+cycle_count = 1
+add_stage = 0
+cycle_record = dict()
 
-print(f"Result is: {INPUT_FILE_NAME}")
+with open(INPUT_FILE_NAME, "r") as input_file:
+    for each_line in input_file:
+        instruction = clean_line = each_line.strip()  # Remove whitespace
+        # print(f"Cycle count {cycle_count:04} : x:{reg_x}")
+        if instruction == "noop":
+            cycle_record[cycle_count] = reg_x
+            cycle_count += 1
+        else:
+            add_parts = instruction.split(" ")
+            assert add_parts[0] == "addx", f"Unexpected instruction: {instruction}"
+            val_to_add = int(add_parts[1])
+            cycle_record[cycle_count] = reg_x
+            cycle_count += 1
+            cycle_record[cycle_count] = reg_x
+            cycle_count += 1
+            reg_x += val_to_add
+
+
+skip_one = False
+sum_of_strengths = 0
+for cycle_num, reg_val in cycle_record.items():
+    # print(f"Cycle count {cycle_num:04} : x:{reg_val}")
+    sig_str = cycle_num * reg_val
+    prev_reg_val = reg_val
+    if (cycle_num % 20) == 0:
+        if skip_one:
+            skip_one = False
+        else:
+            print(f"End cycle {cycle_num:04} : x: {reg_val:04} sig str: {sig_str:04}")
+            sum_of_strengths += sig_str
+            skip_one = True
+    else:
+        # print(f"End cycle {cycle_num:04} : x: {reg_val:04} sig str: {sig_strength:04}")
+        pass
+
+
+print(f"Result is: {sum_of_strengths}")
--- Puzzle_10/puzzle_10_notes.md
+++ Puzzle_10/puzzle_10_notes.md
@@ -6,7 +6,16 @@
 
 [my solution](puzzle_10-part_1_jmt.py)
 
-TBA
+This one isn't overly complicated, but does introduce the likelihood of one of the most common problems in 
+software development, the "off-by-oe" error.
+
+The problem talks about things happening at the start of a cycle, where they are during a cycle, and what 
+happens at the end of a cycle.  None of this is rocket surgery, but if you don't kepe track of it carefully 
+it's easy to be out by a little bit.
+
+I also wasted a bucket load of time on this thinking that the longer example on the web page was the same as 
+the actual puzzle input.  They are about the same size, but not the same.
+
 
 Run with:
 ```python puzzle_10-part_1_jmt.py```
